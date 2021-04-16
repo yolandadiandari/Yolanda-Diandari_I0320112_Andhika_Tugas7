ef3bb9195fbe4b95ab6b9bf56b145b04ccb353af
Yolanda Diandari <yolandadiandari@gmail.com>
2021-04-16 20:33:53 +0700

    update tugas 7

--- I0320112_soal1_tugas7.py
+++ I0320112_soal1_tugas7.py
@@ -1,1 +1,14 @@
+judul = "SOAL 1 TUGAS 7"
+#fungsi 1 : menuliskan teks dalam posisi center
+print(judul.center(50, ' '))
 
+teks = input("Masukkan Teks :")
+
+# fungsi 2 : menampilkan teks dalam huruf kapital
+print("teks dalam huruf kapital :", teks.upper())
+
+#fungsi 3 : menghitung jumlah huruf tertentu
+print("jumlah huruf a dalam teks:", teks.count(input()))
+
+#fungsi 4 : menghitung panjang teks
+print("panjang teks yang dituliskan adalah:", len(teks))
--- I0320112_soal2_tugas7.py
+++ I0320112_soal2_tugas7.py
@@ -1,1 +1,25 @@
+# masukkan jumlah siswa
+x = input("Masukkan data nilai:").split(',')
+
+jumlah = len(x)
+
+for i in range(0,len(x)):
+    x[i] = int(x[i])
+
+import math
+# fungsi 1 : menentukan nilai terendah
+print("Nilai paling rendah adalah:", min(x))
+
+#fungsi 2 : menentukan nilai tertinggi
+print("Nilai tertinggi adalah", max(x))
+
+#fungsi 3 : menentukan rata-rata nilai mahasiswa
+rata_rata = sum(x)/jumlah
+
+#fungsi 4 : pembulatan ke bawah nilai rata-rata
+print("Nilai rata-rata : ", math.floor(rata_rata))
+
+#mengurutkan nilai
+print("Nilai terendah ke tertinggi adalah", sorted(x))
+
 
